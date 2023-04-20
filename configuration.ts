46f8912ff9ee400a3cf36b21579d4bd98d6d9e3d
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2023-04-20 17:28:54 -0400

    release: v0.2.5 with ListRelations and improved parallization

--- configuration.ts
+++ configuration.ts
@@ -66,7 +66,7 @@ export class Configuration {
    * @type {string}
    * @memberof Configuration
    */
-  private static sdkVersion = "0.2.4";
+  private static sdkVersion = "0.2.5";
 
   /**
    * provide scheme (e.g. `https`)
