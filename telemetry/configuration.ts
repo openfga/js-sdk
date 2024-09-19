997b0ae2231926c5887e622d73887945ae49ffc0
Jim Anderson <jim.anderson@okta.com>
2024-09-19 13:25:57 -0500

    add jsdocs to TelemetryConfig

--- telemetry/configuration.ts
+++ telemetry/configuration.ts
@@ -2,22 +2,55 @@ import { FgaValidationError } from "../errors";
 import { TelemetryAttribute } from "./attributes";
 import { TelemetryMetric } from "./metrics";
 
+/**
+ * Configuration for a telemetry metric.
+ * 
+ * @interface TelemetryMetricConfig
+ * @property {Set<TelemetryAttribute>} attributes - A set of attributes associated with the telemetry metric.
+ */
 export interface TelemetryMetricConfig {
   attributes: Set<TelemetryAttribute>;
 }
 
+/**
+ * Represents the overall configuration for telemetry, including various metrics.
+ * 
+ * @interface TelemetryConfig
+ * @property {Record<TelemetryMetric, TelemetryMetricConfig>} metrics - A record mapping telemetry metrics to their configurations.
+ */
 export interface TelemetryConfig {
   metrics: Record<TelemetryMetric, TelemetryMetricConfig>;
 }
 
+/**
+ * Provides the configuration for a specific telemetry metric.
+ * 
+ * @class TelemetryMetricConfiguration
+ * @implements {TelemetryMetricConfig}
+ * @param {Set<TelemetryAttribute>} [attributes=TelemetryConfiguration.defaultAttributes] - A set of attributes for the metric. Defaults to the standard attributes.
+ */
 export class TelemetryMetricConfiguration implements TelemetryMetricConfig {
   constructor(
     public attributes: Set<TelemetryAttribute> = TelemetryConfiguration.defaultAttributes
   ) {}
 }
 
+/**
+ * Manages the overall telemetry configuration, including default and valid attributes.
+ * 
+ * @class TelemetryConfiguration
+ * @implements {TelemetryConfig}
+ */
 export class TelemetryConfiguration implements TelemetryConfig {
-  public static readonly defaultAttributes = new Set<TelemetryAttribute>([
+
+  /**
+   * Default attributes for telemetry metrics.
+   * 
+   * @static
+   * @readonly
+   * @type {Set<TelemetryAttribute>}
+   */
+  public static readonly defaultAttributes: Set<TelemetryAttribute> = new Set<TelemetryAttribute>([
     TelemetryAttribute.HttpHost,
     TelemetryAttribute.HttpResponseStatusCode,
     TelemetryAttribute.UserAgentOriginal,
@@ -27,7 +60,7 @@ export class TelemetryConfiguration implements TelemetryConfig {
     TelemetryAttribute.FgaClientRequestModelId,
     TelemetryAttribute.HttpRequestResendCount,
     TelemetryAttribute.FgaClientResponseModelId,
-  
+ 
     // These metrics are not included by default because they are usually less useful
     // TelemetryAttribute.UrlScheme,
     // TelemetryAttribute.HttpRequestMethod,
@@ -38,8 +71,15 @@ export class TelemetryConfiguration implements TelemetryConfig {
     // This not included by default as it has a very high cardinality which could increase costs for users
     // TelemetryAttribute.FgaClientUser
   ]);
-  
-  public static readonly validAttriburtes = new Set<TelemetryAttribute>([
+
+  /**
+   * Valid attributes that can be used in telemetry metrics.
+   * 
+   * @static
+   * @readonly
+   * @type {Set<TelemetryAttribute>}
+   */
+  public static readonly validAttriburtes: Set<TelemetryAttribute> = new Set<TelemetryAttribute>([
     TelemetryAttribute.HttpHost,
     TelemetryAttribute.HttpResponseStatusCode,
     TelemetryAttribute.UserAgentOriginal,
@@ -54,9 +94,14 @@ export class TelemetryConfiguration implements TelemetryConfig {
     TelemetryAttribute.UrlFull,
     TelemetryAttribute.HttpClientRequestDuration,
     TelemetryAttribute.HttpServerRequestDuration,
-    TelemetryAttribute.FgaClientUser
+    TelemetryAttribute.FgaClientUser,
   ]);
 
+  /**
+   * Creates an instance of TelemetryConfiguration.
+   * 
+   * @param {Record<TelemetryMetric, TelemetryMetricConfig>} [metrics] - A record mapping telemetry metrics to their configurations.
+   */
   constructor(
     public metrics: Record<TelemetryMetric, TelemetryMetricConfig> = {
       [TelemetryMetric.CounterCredentialsRequest]: new TelemetryMetricConfiguration(),
@@ -65,27 +110,32 @@ export class TelemetryConfiguration implements TelemetryConfig {
     },
   ) {}
 
+  /**
+   * Validates that the configured metrics use only valid attributes.
+   * 
+   * @throws {FgaValidationError} Throws an error if any attribute in the metric configurations is invalid.
+   */
   public ensureValid(): void {
     const validAttrs = TelemetryConfiguration.validAttriburtes;
 
-    const counterConfigAttrs = this.metrics.counterCredentialsRequest?.attributes || new Set<TelemetryAttribute>;
+    const counterConfigAttrs = this.metrics.counterCredentialsRequest?.attributes || new Set<TelemetryAttribute>();
     counterConfigAttrs.forEach(counterConfigAttr => {
       if (!validAttrs.has(counterConfigAttr)) {
-        throw new FgaValidationError(`Configuration.telemtry.metrics.counterCredentialsRequest attribute '${counterConfigAttrs}' is not a valid attribute`);
+        throw new FgaValidationError(`Configuration.telemetry.metrics.counterCredentialsRequest attribute '${counterConfigAttr}' is not a valid attribute`);
       }
     });
 
-    const histogramRequestDurationConfigAttrs = this.metrics.histogramRequestDuration?.attributes || new Set<TelemetryAttribute>;
+    const histogramRequestDurationConfigAttrs = this.metrics.histogramRequestDuration?.attributes || new Set<TelemetryAttribute>();
     histogramRequestDurationConfigAttrs.forEach(histogramRequestDurationAttr => {
       if (!validAttrs.has(histogramRequestDurationAttr)) {
-        throw new FgaValidationError(`Configuration.telemtry.metrics.histogramRequestDuration attribute '${histogramRequestDurationAttr}' is not a valid attribute`);
+        throw new FgaValidationError(`Configuration.telemetry.metrics.histogramRequestDuration attribute '${histogramRequestDurationAttr}' is not a valid attribute`);
       }
     });
 
-    const histogramQueryDurationConfigAttrs = this.metrics.histogramQueryDuration?.attributes || new Set<TelemetryAttribute>;
+    const histogramQueryDurationConfigAttrs = this.metrics.histogramQueryDuration?.attributes || new Set<TelemetryAttribute>();
     histogramQueryDurationConfigAttrs.forEach(histogramQueryDurationConfigAttr => {
       if (!validAttrs.has(histogramQueryDurationConfigAttr)) {
-        throw new FgaValidationError(`Configuration.telemtry.metrics.histogramQueryDuration attribute '${histogramQueryDurationConfigAttrs}' is not a valid attribute`);
+        throw new FgaValidationError(`Configuration.telemetry.metrics.histogramQueryDuration attribute '${histogramQueryDurationConfigAttr}' is not a valid attribute`);
       }
     });
   }
