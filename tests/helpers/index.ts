38a3a328d8f13e2022588488335c9bc1242e43ce
Abishek Newar <97790157+Abishek-Newar@users.noreply.github.com>
2026-02-11 08:23:26 +0530

    undo the chanegs in nocks and chages in index.ts

--- tests/helpers/index.ts
+++ tests/helpers/index.ts
@@ -1,2 +1,2 @@
 export { getNocks } from "./nocks";
-export { baseConfig, defaultConfiguration, OPENFGA_STORE_ID } from "./default-config";
+export { baseConfig, defaultConfiguration } from "./default-config";
