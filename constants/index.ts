341b99a1298e703f447d4ffaf51d328405f4bb70
Anurag Bandyopadhyay <angbpy@gmail.com>
2026-03-30 17:19:16 +0530

    feat: release automation config

--- constants/index.ts
+++ constants/index.ts
@@ -14,7 +14,7 @@
 /**
  * Version of the OpenFGA JavaScript SDK.
  */
-const SdkVersion = "0.9.3";
+const SdkVersion = "0.9.3"; // x-release-please-version
 
 /**
  * User agent used in HTTP requests.
