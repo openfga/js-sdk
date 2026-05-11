2b3126f1c88b222bd0746c1f87725625b4bf6937
Kamil Ogórek <kamil.ogorek@gmail.com>
2026-05-11 17:01:54 +0200

    Revert "remove unnecessary enableNetConnect"
    This reverts commit 7bbf439b3d8d7c23ea2df6dd2ba422e781320a7e.

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,7 +1,13 @@
 import nock from "nock";
 
+// NOTE: Do not replace host constant with `OPENFGA_API_URL` import from `helpers/default-config`,
+// as this very file imports `ClientConfiguration` class, and this in turn will invalidate
+// jest's mock hoisting and break all telemetry tests.
+const OPENFGA_API_URL_HOST = "api.fga.example";
+
 beforeEach(() => {
   nock.disableNetConnect();
+  nock.enableNetConnect((host) => host.startsWith(OPENFGA_API_URL_HOST));
 });
 
 afterEach(() => {
