7bbf439b3d8d7c23ea2df6dd2ba422e781320a7e
Kamil Ogórek <kamil.ogorek@gmail.com>
2026-05-11 09:53:51 +0200

    remove unnecessary enableNetConnect

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,13 +1,7 @@
 import nock from "nock";
 
-// NOTE: Do not replace host constant with `OPENFGA_API_URL` import from `helpers/default-config`,
-// as this very file imports `ClientConfiguration` class, and this in turn will invalidate
-// jest's mock hoisting and break all telemetry tests.
-const OPENFGA_API_URL_HOST = "api.fga.example";
-
 beforeEach(() => {
   nock.disableNetConnect();
-  nock.enableNetConnect((host) => host.startsWith(OPENFGA_API_URL_HOST));
 });
 
 afterEach(() => {
